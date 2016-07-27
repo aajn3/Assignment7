a1da83050b7e25153878f2662d101463e8571001
Arlina Ramrattan <arlina.ramrattan@gmail.com>
2016-07-27 11:16:18 -0400

    Comments and deleting console writelines

--- Form1.cs
+++ Form1.cs
@@ -69,11 +69,13 @@ namespace Assignment6_7GuessingGame
             changes = game.CheckGuess(cellValue);
 
             //if statement to check if there are still lives
-            if (Convert.ToInt32(changes[3]) <= 0)
+           
+            if (Convert.ToInt32(changes[3]) <= 0) //losing
             {
+                //adding text values to the controls
                 string text = changes[0] + "\n" + changes[1];
                 txtBoxMessage.Text = text.Replace("\n", System.Environment.NewLine); 
-                Console.WriteLine(text);
+                
                 lblScore.Text = "Score: " + changes[2];
                 lblLives.Text = "Lives: " + changes[3];
                 //this will change the label color
@@ -81,8 +83,9 @@ namespace Assignment6_7GuessingGame
                 lbl.BackColor = col;
                 EndGame();
             }
-            else if (changes[0] == "CONGRATULATIONS! YOU WIN!")
+            else if (changes[0] == "CONGRATULATIONS! YOU WIN!")//winning
             {
+                //adding text values to the controls
                 string text = changes[0] + "\nScore: " + changes[2] + "\nLives: " + changes[3] + " \n" + changes[1];
                 txtBoxMessage.Text = text.Replace("\n", System.Environment.NewLine);
                 
@@ -94,7 +97,7 @@ namespace Assignment6_7GuessingGame
                 lbl.BackColor = col;
                 EndGame();
             }
-            else
+            else //default (less than random and greater than random)
             {
                 // changing the text values to match the picked values
                 txtBoxMessage.Text = changes[0];
@@ -125,15 +128,16 @@ namespace Assignment6_7GuessingGame
             Label l = ((Label)sender);
             previousBgColour = l.BackColor;
             //calling the ClickGuess function and passing through the value of the label and the label itself
+            //if is checking to see if the number was chosen by checking the back color
             if (l.BackColor.Equals(System.Drawing.SystemColors.Control))
             {
                 ClickGuess(Convert.ToInt32(l.Text), l);
-               // Console.WriteLine(l.Text);
+               
             }
             else
                 MessageBox.Show("Please choose a number that has not already been chosen");
 
-            //l.BackColor = Color.FromArgb(0, 0, 255);
+           
         }
 
         private void btnPlayAgain_Click(object sender, EventArgs e)
